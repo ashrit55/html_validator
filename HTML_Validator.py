117b1e926139d4ea381790a0ba76619a8e8f7d6b
ashrit55 <74885989+ashrit55@users.noreply.github.com>
2023-08-10 14:54:39 -0700

    Update HTML_Validator.py

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,6 +1,6 @@
 def validate_html(html):
     '''
-    
+
     This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
@@ -14,25 +14,21 @@ def validate_html(html):
     # then process these html tags using the balanced parentheses algorithm from the class/book
     # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
-    newl =  _extract_tags(html)
+    newl = _extract_tags(html)
     stack = []
 
-    
-    for c  in newl:
-        for i in c: 
+    for c in newl:
+        for i in c:
             if i == "<":
                 stack.append("<")
             else:
-                if i == ">" and stack.pop() != "<" :
+                if i == ">" and stack.pop() != "<":
                     print(False)
-                
+
     if len(stack) > 0:
-        print( False)
+        print(False)
     else:
         print(True)
-
-
-
 
 
 def _extract_tags(html):
@@ -49,24 +45,23 @@ def _extract_tags(html):
     myl = []
     news = ""
     i = 0
-    while i < len(html): 
+    while i < len(html):
         if html[i:i+1] == "<":
             while html[i:i+1] != ">" and i < len(html):
 
                 news = news + html[i:i+1]
-                i+=1
+                i += 1
             if i < len(html):
-                news+= ">"
+                news += ">"
             myl.append(news)
-        else: 
+        else:
             news = ""
-            i+=1
+            i += 1
     print(myl)
-    return(myl)
+    return (myl)
 
 
-#_extract_tags('Python <strong>rocks</strong>!')
-#validate_html('<strong>example</strong>')
-#_extract_tags('<a><b><c></a></b><f>')
-#validate_html('this is a <strong< test')
-
+# _extract_tags('Python <strong>rocks</strong>!')
+# validate_html('<strong>example</strong>')
+# _extract_tags('<a><b><c></a></b><f>')
+# validate_html('this is a <strong< test')
